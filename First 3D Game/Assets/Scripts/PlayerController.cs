af1f7d30bf66f4383a6a7bf559478645cd494ba5
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-03-14 11:07:40 -0700

    Yahoo!

---
+++ First 3D Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _moveSpeed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float xValue = Input.GetAxis("Horizontal") * _moveSpeed*Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * _moveSpeed*Time.deltaTime;
+
+        transform.Translate(xValue, 0f, zValue);
+    }
+}
