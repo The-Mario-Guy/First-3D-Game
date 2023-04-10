1f8124629f2887a9f766ba7a7751645a26d3ad08
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-04-10 12:36:14 -0700

    Garf
    Lasang

---
+++ First 3D Game/Assets/Scenes/New Game/Scripts/PlayerTWOControlls.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTWOControlls : MonoBehaviour
+{
+    public float _moveSpeed = 20;
+     public int forceConst = 500;
+     
+    private Rigidbody selfRigidbody;
+     public GameObject player;
+    public float sensitivity;
+    float star = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         selfRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         {
+         float rotateHorizontal = Input.GetAxis ("Mouse X");
+         float rotateVertical = Input.GetAxis ("Mouse Y");
+         transform.RotateAround (player.transform.position, -Vector3.up, rotateHorizontal * sensitivity);
+        transform.RotateAround (Vector3.zero, transform.right, rotateVertical * sensitivity);
+        }
+    }
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float xValue = Input.GetAxis("Horizontal") * _moveSpeed*Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * _moveSpeed*Time.deltaTime;
+
+        transform.Translate(xValue, 0f, zValue);
+    }
+}
+
--- First 3D Game/Assets/Scripts/FPSLimit.cs
+++ First 3D Game/Assets/Scripts/FPSLimit.cs
@@ -7,7 +7,7 @@ public class FPSLimit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 20;
     }
 
     // Update is called once per frame
